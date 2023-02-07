b615616cc16b0964fe622773b5d242ac0ad9d75b
vannhgch211217 <110003760+vannhgch211217@users.noreply.github.com>
2023-02-07 10:27:40 +0700

    linktopage

--- main2.js
+++ main2.js
@@ -3,13 +3,15 @@ var app = express();
 app.get('/', function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('<html><body><p>This is home Page.</p></body></html>');
-        res.end();
+        res.end('<p><a href="/student">Click here!</a></p>\n');
+        // res.end();
 })
 
 app.get('/student', function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<html><body><p>This is student Page.</p></body></html>');
-        res.end();
+        res.end('<p><a href="/">Click here!</a></p>\n');
+
 })
 
 const PORT = process.env.PORT || 8000
